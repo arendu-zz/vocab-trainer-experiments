914a202880e2896b6d3e025ae74abe7a422e157d
arendu <adithya.r@gmail.com>
2017-02-08 18:12:52 -0500

    simple model for scalar and vector manupulation of gradients

--- code/simple_loglinear.py
+++ code/simple_loglinear.py
@@ -1,9 +1,10 @@
 ##!/usr/bin/env python
 import numpy as np
 from optimizers import sgd
-from optimizers import rmsprop
+from optimizers import momentum
 import theano
 import theano.tensor as T
+
 
 __author__ = 'arenduchintala'
 
@@ -15,19 +16,28 @@ else:
     floatX = np.float64
 
 class SimpleLoglinear(object):
-    def __init__(self, dh, regularization = 0.1, learner_regularization=0.5):
+    def __init__(self, dh, reg = 0.1, learner_reg = 0.5, adapt = False, x1 = 1.0, x2 = 0.2, clip = False):
         self.dh = dh #DataHelper(event2feats_file, feat2id_file, actions_file)
-        self.l = regularization #regularization parameter
-        self.ul = learner_regularization #regularization parameter for the learner model
-        self._eps = np.finfo(np.float32).tiny #1e-10 # for fixing divide by 0
+        self.adapt = adapt
+        self.clip = clip
+        self.l = reg #reg parameter
+        self.ul = learner_reg #reg parameter for the learner model
+        self._eps = np.finfo(np.float32).eps #1e-10 # for fixing divide by 0
         self._mult_eps = np.finfo(np.float32).eps #1e-10 # for fixing divide by 0
         self.phi = theano.shared(floatX(self.load_phi()), name='Phi') #(output_dim, feat_size)
-        x = np.zeros((2,))
-        x[0] = 0.9
-        x[1] = 0.1
-        self.W_lr = theano.shared(floatX(x), name='W_lr')
-        self.params = [self.W_lr]
-        self.reg_params = [self.W_lr]
+        if self.adapt:
+            x = 5 * np.ones((self.dh.FEAT_SIZE,))
+            self.b_z = theano.shared(floatX(x), name='b_z')
+            self.b_r = theano.shared(floatX(x), name='b_z')
+            self.W_r = theano.shared(floatX(x1 * x), name='W_r')
+            self.W_z = theano.shared(floatX(x2 * x), name='W_z')
+            self.params = [self.W_r, self.b_r, self.W_z, self.b_z]
+        else:
+            x = 1.0
+            self.W_r = theano.shared(floatX(x1 * x), name='W_r')
+            self.W_z = theano.shared(floatX(x2 * x), name='W_z')
+            self.params = [self.W_r, self.W_z]
+        self.reg_params = [self.W_r, self.W_z]
         self.make_graph()
 
     def _phi(self, f_idx, e_idx):
@@ -46,13 +56,20 @@ class SimpleLoglinear(object):
     def make_graph(self):
         lr = T.scalar('lr', dtype=theano.config.floatX) # learning rate scalar
         X = T.ivector('X') #(sequence_size,) #index of the input string
-        O = T.imatrix('O') #(sequence_size, output_dim) #mask for possible Ys
-        Y = T.imatrix('Y') #(sequence_size, output_dim) #the Y that is selected by the user
+        O = T.fmatrix('O') #(sequence_size, output_dim) #mask for possible Ys
+        Y = T.fmatrix('Y') #(sequence_size, output_dim) #the Y that is selected by the user
         S = T.fmatrix('S') #(sequence_size,7) # was the answer marked as correct or incorrect?
         theta_0 = T.fvector('theta_0') #(feature_size,)
 
         def rms(v):
             return T.sqrt(v + self._eps)
+
+        def bin_loss(y_selected, y_predicted, feedback):
+            yp_unsafe = T.switch(y_selected,y_predicted,0.0)
+            yp = T.clip(yp_unsafe, self._eps, 0.9999999)
+            l = T.switch(feedback[4,7], -T.log(yp), -T.log(1.0 - yp))
+            l = T.switch(feedback[3], 0, l)
+            return l
 
         def create_target(y_selected, y_predicted, feedback):
             #y_selected is a one-hot vector
@@ -69,18 +86,25 @@ class SimpleLoglinear(object):
             a_m = T.switch(mask, a, val)
             return a_m
 
+        def assign_losses(loss_t, s_t):
+            r_loss_t = T.switch(s_t[3], loss_t, 0)
+            c_loss_t = T.switch(T.any(s_t[[4,7]]), loss_t, 0)
+            ic_loss_t = T.switch(T.any(s_t[[5,8]]), loss_t, 0)
+            return r_loss_t, c_loss_t, ic_loss_t
+
         def log_linear_t(Phi_x_t, y_t, o_t, s_t, theta_t):
             y_dot = Phi_x_t.dot(theta_t.T) #(Y,D,) dot (D,)
-            y_dot_masked = masked(y_dot, o_t, -np.inf) #(Y,)
+            y_dot_masked = masked(y_dot, o_t, -100000000.0000) #(Y,)
             y_hat_unsafe  = T.nnet.softmax(y_dot_masked) #(Y,)
             y_hat = T.clip(y_hat_unsafe, self._eps, 0.9999999)
+            model_loss = -T.sum(y_t * T.log(y_hat))
             y_target = create_target(y_t, y_hat, s_t)
-            ll_loss_vec = -T.sum(y_target * T.log(y_hat), axis=1)  #cross-entropy
-            ll_loss = T.mean(ll_loss_vec) #+ (self.l * reg)
-            theta_t_grad = y_target.dot(Phi_x_t) - y_hat.dot(Phi_x_t) # - (self.ul * 2.0 * theta_t) #obs - exp
+            #user_loss = -T.sum(y_target * T.log(y_hat), axis=1) + self.ul * T.sum(T.sqr(theta_t))
+            #theta_t_grad = T.grad(user_loss, theta_t)
+            theta_t_grad = y_target.dot(Phi_x_t) - y_hat.dot(Phi_x_t)  - (self.ul * 2.0 * theta_t) #obs - exp
             theta_t_grad = T.reshape(theta_t_grad, theta_t.shape) #(D,)
             y_hat = T.reshape(y_hat, (self.dh.E_SIZE,)) #(Y,)
-            return theta_t_grad, y_hat, ll_loss
+            return theta_t_grad, y_hat, model_loss
 
         def recurrence(x_t, y_t, o_t, s_t, theta_t):
             #x_t (scalar)
@@ -89,31 +113,47 @@ class SimpleLoglinear(object):
             #s_t (9,)
             #theta_t (D,)
             s_t = T.reshape(s_t, (9,))
+            theta_t = T.reshape(theta_t, (self.dh.FEAT_SIZE,))
             Phi_x_t = self.phi[x_t, :, :] #(1, Y, D)
             Phi_x_t = T.reshape(Phi_x_t, (self.dh.E_SIZE, self.dh.FEAT_SIZE)) #(Y,D)
             grad_theta_t, y_hat, loss_t = log_linear_t(Phi_x_t, y_t, o_t, s_t, theta_t) #(D,) and scalar
-            #theta_tp1 = T.clip(theta_t + self.lr * grad_theta_tm1, -1.0, 1.0)
-            #adapt_grad_t, E_g_tp1, E_dx_tp1 = adapt_grad(grad_theta_t, E_g_t, E_dx_t)
-            #theta_tp1 = theta_t - self.lr * grad_theta_tm1
-            #reg = 2 * theta_t  #TODO:add to gradient..
+            r_loss_t, c_loss_t, ic_loss_t = assign_losses(loss_t, s_t)
             if s_t[6] == 1: #nofeeback no knowledge change...
                 theta_tp1 = theta_t
             else:
-                p = T.nnet.sigmoid(self.W_lr)
-                theta_tp1 = (p[0] * theta_t) + (p[1] * grad_theta_t)
-            return theta_tp1, y_hat, loss_t
+                if self.adapt:
+                    theta_tp1 = T.nnet.sigmoid(self.W_r + self.b_r) * theta_t + T.nnet.sigmoid(self.W_z + self.b_z) * grad_theta_t
+                else:
+                    theta_tp1 = T.nnet.sigmoid(self.W_r) * theta_t + T.nnet.sigmoid(self.W_z) * grad_theta_t
 
-        [seq_thetas, seq_y_hats, seq_losses], _ = theano.scan(fn=recurrence, sequences=[X,Y,O,S], outputs_info=[theta_0, None, None])
-        seq_loss = T.mean(seq_losses) 
+            if self.clip:
+                theta_tp1 = T.clip(theta_tp1, -1.0, 1.0)
+            else:
+                pass
+            return theta_tp1, y_hat, loss_t, r_loss_t,c_loss_t, ic_loss_t
+
+        [seq_thetas, seq_y_hats, all_losses, r_losses, c_losses, ic_losses], _ = theano.scan(fn=recurrence, 
+                sequences=[X,Y,O,S], 
+                outputs_info=[theta_0, None, None, None, None, None])
+
+        all_loss = T.sum(all_losses)
+        r_loss = T.mean(T.nonzero_values(r_losses))
+        c_loss = T.mean(T.nonzero_values(c_losses))
+        ic_loss = T.mean(T.nonzero_values(ic_losses))
         reg_loss = 0.0
         for reg_param in self.reg_params:
+            reg_loss += T.sum(T.abs_(reg_param + self._eps))
             reg_loss += T.sum(T.sqr(reg_param))
-        total_loss = seq_loss + (self.l * reg_loss)
-        self.get_seq_losses = theano.function([X, Y, O, S, theta_0], outputs = seq_losses)
+        total_loss = ic_loss + (self.l * reg_loss)
+        self.get_seq_loss = theano.function([X, Y, O, S, theta_0], outputs = all_loss)
         self.get_params = theano.function(inputs = [], outputs = self.params)
-        self.get_seq_loss = theano.function([X, Y, O, S, theta_0], outputs = seq_loss)
-        self.get_total_loss = theano.function([X, Y, O, S, theta_0], outputs = total_loss)
+        self.get_seq_losses = theano.function([X, Y, O, S, theta_0], outputs = [all_losses, r_losses, c_losses, ic_losses])
+        self.get_loss = theano.function([X, Y, O, S, theta_0], outputs = [total_loss, all_loss, r_loss, c_loss, ic_loss])
         self.get_seq_y_hats = theano.function([X, Y, O, S, theta_0], outputs = seq_y_hats)
         self.get_seq_thetas = theano.function([X, Y, O, S, theta_0], outputs = seq_thetas)
-        self.do_sgd_update = theano.function([X, Y, O, S, theta_0, lr], outputs= [seq_loss, seq_thetas, seq_y_hats], updates = rmsprop(total_loss, self.params, lr))
-        self.do_rms_update = theano.function([X, Y, O, S, theta_0, lr], outputs= [seq_loss, seq_thetas, seq_y_hats], updates = sgd(total_loss, self.params, lr))
+        self.do_sgd_update = theano.function([X, Y, O, S, theta_0, lr], 
+                outputs= [total_loss, seq_thetas, seq_y_hats], 
+                updates = momentum(total_loss, self.params, lr))
+        self.do_rms_update = theano.function([X, Y, O, S, theta_0, lr], 
+                outputs= [total_loss, seq_thetas, seq_y_hats], 
+                updates = sgd(total_loss, self.params, lr))
