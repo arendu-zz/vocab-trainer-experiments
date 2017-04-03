3a95449472a87eaf53b1cd66cefd8a757fe1efdb
arendu <adithya.r@gmail.com>
2017-04-03 01:29:07 -0400

    added experience

--- step_simple.py
+++ step_simple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#dd_!/usr/bin/env python
 import argparse
 import sys
 import codecs
@@ -8,6 +8,7 @@ from code.data_reader import read_data
 from code.datahelper import DataHelper
 from code.recurrent_loglinear import RecurrentLoglinear
 from code.rewards import Reward
+from code.dqn import DQNTheano as DQN
 #from code.eval_tools import disp_eval
 #import pdb
 
@@ -78,6 +79,42 @@ def get_observation(action_idx, rll, theta_tm1, disp=False):
     y_sel_vec[y_sel] = 1.0
     return y_sel, y_sel_vec, y_hat, context_t
 
+def merge(belief_weights, states):
+    b = np.zeros_like(states[0])
+    for k_idx, (bw, s) in enumerate(zip(belief_weights, states)):
+        b += bw * s
+    return b
+
+def add_to_experince(B_t, a_t, r_t, B_tm1):
+    global B_t_exp, B_tm1_exp, A_t_exp, R_t_exp
+    assert B_t.shape == B_tm1.shape
+    assert B_t.shape == (dh.FEAT_SIZE,)
+    if B_t_exp is None:
+        B_t_exp = B_t
+    else:
+        B_t_exp = np.vstack((B_t_exp, B_t))
+
+    if B_tm1_exp is None:
+        B_tm1_exp = B_tm1
+    else:
+        B_tm1_exp = np.vstack((B_tm1_exp, B_tm1))
+
+    if A_t_exp is None:
+        A_t_exp = a_t
+    else:
+        A_t_exp = np.hstack((A_t_exp, a_t))
+
+    if R_t_exp is None:
+        R_t_exp = r_t
+    else:
+        R_t_exp = np.hstack((R_t_exp, r_t)
+
+def get_batch_experience(bs = 1000):
+    pass
+
+def remove_from_experince(limit = 100000):
+    pass
+
 
 if __name__ == '__main__':
     np.random.seed(124)
@@ -96,38 +133,56 @@ if __name__ == '__main__':
     quiz_actions_file = './data/content/fake-en-medium.mc.tp.mcr.tpr.actions.test'
     dh = DataHelper(events_file, feats_file, actions_file, quiz_actions_file)
     reward = Reward(dh)
+    dqn = DQN([dh.FEAT_SIZE, np.int64(0.33 * dh.FEAT_SIZE), np.int64(0.1 * dh.FEAT_SIZE), len(dh.actions)])  
     TRAINING_SEQ = read_data(options.training_data, dh)
     DEV_SEQ = read_data(options.dev_data, dh)
     T_SEQ = read_data(options.test_data, dh)
     K_files = codecs.open(options.K_saved_models, 'r', 'utf8').readlines()
     J_files = codecs.open(options.J_saved_models, 'r', 'utf8').readlines()
-    print 'k'
+    print 'loading Ks:'
     rll_Ks = [load_json_model(l.strip()) for l in K_files[:] if l.strip() != '']
-    print 'j'
+    print 'loading Js'
     rll_Js = [load_json_model(l.strip()) for l in J_files[:] if l.strip() != '']
     theta_tm1_Ks = [np.zeros((dh.FEAT_SIZE,)).astype(floatX) for _ in rll_Ks]
     belief_weight_Ks = (1.0 / len(rll_Ks))  * np.ones(len(rll_Ks))
-    #TODO: setup experience replay memory
+    B_tm1 = merge(belief_weight_Ks, theta_tm1_Ks) 
+    eps = 0.4
+
     #while not converged
     rll_j = np.random.choice(rll_Js, 1)[0]
     print 'sampled:', rll_j.saved_weights, 'jth model'
     theta_tm1_j = np.zeros(dh.FEAT_SIZE,).astype(floatX)
     context_tm1 = np.zeros(10,).astype(floatX)
-    for _ in range(10): # each training step t = [1:T]
-        print 'training step', _
-        action_idx = int(raw_input('pick action 0-' + str(len(dh.actions) - 1))) #15 #np.random.choice(len(dh.action_vectors), 1)[0]
-        a_type, _x_t, _yt_t, _o_t = dh.action_vectors[action_idx]
-        y_sel_j, y_sel_j_vec, y_hat_j, context_t = get_observation(action_idx, rll_j, theta_tm1_j, disp=True)
+    for _ in range(20): # each training step t = [1:T]
+        if np.random.rand() < eps:
+            action_idx_t = int(raw_input('pick action 0-' + str(len(dh.actions) - 1))) #15 #np.random.choice(len(dh.action_vectors), 1)[0]
+        else:
+            q_hat = dqn.get_Q_hat(B_tm1)
+            action_idx_t = np.argmax(q_hat)
+
+        a_type, _x_t, _yt_t, _o_t = dh.action_vectors[action_idx_t]
+
+        y_sel_j, y_sel_j_vec, y_hat_j, context_t = get_observation(action_idx_t, rll_j, theta_tm1_j, disp=True)
         theta_t_j, g_r_j, g_z_j = rll_j.get_step_transition(_x_t, y_sel_j_vec.astype(floatX), _o_t.astype(floatX), context_t.astype(floatX), context_tm1.astype(floatX), theta_tm1_j.astype(floatX))
         context_tm1 = context_t
         theta_tm1_j = theta_t_j
+
+        reward_Ks = np.zeros(len(rll_Ks),)
         for k_idx, (rll_k, theta_tm1_k) in enumerate(zip(rll_Ks, theta_tm1_Ks)):
-            _, _, y_hat_k, _ = get_observation(action_idx, rll_k, theta_tm1_k)
+            _, _, y_hat_k, _ = get_observation(action_idx_t, rll_k, theta_tm1_k)
             belief_weight_Ks[k_idx] = y_hat_k[y_sel_j] * belief_weight_Ks[k_idx]
             _theta_t_k, _g_r_k, _g_z_k = rll_k.get_step_transition(_x_t, y_sel_j_vec.astype(floatX), _o_t.astype(floatX), context_t.astype(floatX), context_tm1.astype(floatX), theta_tm1_k.astype(floatX))
-            _reward_t_k = reward.get_ll_reward(rll_k, _theta_t_k)
-            print '.'.join(rll_k.saved_weights.split('/')[-1].split('.')[-4:]), k_idx, ' prob on obs=', y_hat_k[y_sel_j], 'reward=', _reward_t_k
+            _reward_ll_t_k = reward.get_ll_reward(rll_k, _theta_t_k)
+            _reward_match_t_k = reward.get_match_reward(rll_k, _theta_t_k)
+            reward_Ks[k_idx] = _reward_ll_t_k
+            print '.'.join(rll_k.saved_weights.split('/')[-1].split('.')[-4:]), k_idx, ' prob on obs=', y_hat_k[y_sel_j], 'reward=', _reward_ll_t_k, _reward_match_t_k
             theta_tm1_Ks[k_idx] = _theta_t_k
         belief_weight_Ks = belief_weight_Ks * (1.0 / np.sum(belief_weight_Ks))
-        print np.sum(belief_weight_Ks)
         assert np.abs(np.sum(belief_weight_Ks) - 1.0) < 1e-10
+        r_t = np.sum([b * r for b,r in zip(belief_weight_Ks, reward_Ks)])
+        B_t = merge(belief_weight_Ks, theta_tm1_Ks) 
+        add_to_experince(B_t, action_idx_t, r_t, B_tm1)
+    print 'final quiz'
+    _reward_match_t_j = reward.get_match_reward(rll_j, theta_t_j)
+    _reward_ll_t_j = reward.get_ll_reward(rll_j, theta_t_j)
+    print "reward for 'real' simulated user match:", _reward_match_t_j, "ll:", _reward_ll_t_j
